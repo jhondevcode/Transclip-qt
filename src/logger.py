6c8df3386615add68dee8e138031350f37fc9e42
jhondevcode <jhondev.code@gmail.com>
2021-09-22 21:54:51 -0500

    New logger definition using loggin api

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+import os
+
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+
+if not os.path.isdir(LOG_DIR):
+    os.mkdir(LOG_DIR)
+
+log_file = os.path.join(LOG_DIR, f"log-{datetime.datetime.now().date()}.log")
+logging.basicConfig(filename=log_file, format='%(asctime)s %(message)s', filemode='a')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def info(msg, *args, **kwargs):
+    logger.info(msg, args, kwargs)
+
+
+def warning(msg, *args, **kwargs):
+    logger.warning(msg, args, kwargs)
+
+
+def error(msg, *args, **kwargs):
+    logger.error(msg, args, kwargs)
