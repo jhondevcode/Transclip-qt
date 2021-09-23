ac6c6ea11c8dd10814fcfb7c6e43e61eabb632e7
jhondevcode <jhondev.code@gmail.com>
2021-09-23 09:43:47 -0500

    Information constants have been added

---
+++ src/constant.py
@@ -0,0 +1,7 @@
+PROGRAM_NAME = "Transclip"
+PROGRAM_AUTHOR = "Jhon Fernandez"
+PROGRAM_LICENSE = "GPL-3.0"
+PROGRAM_COPYRIGHT = "Copyright 2021 jhondevcode"
+PROGRAM_DESCRIPTION = "A small utility to translate clipboard content"
+PROGRAM_VERSION = "1.0.0"
+PROGRAM_URL = "https://github.com/jhondevcode/transclip-qt"
