e6452aaa7ee03eace17fb1894d8969352b10e762
jhondevcode <jhondev.code@gmail.com>
2021-10-27 12:25:09 -0500

    Unnecessary imports and functions removed

--- src/transclip.py
+++ src/transclip.py
@@ -2,16 +2,9 @@ import os
 import sys
 from PyQt6.QtWidgets import QApplication
 from widgets import MainWindow
-from util import WORKSPACE
-
-
-def prepare_workspace():
-    if os.getcwd() != WORKSPACE:
-        os.chdir(WORKSPACE)
 
 
 def main():
-    prepare_workspace()
     app = QApplication(sys.argv)
     window = MainWindow(app)
     window.show()
