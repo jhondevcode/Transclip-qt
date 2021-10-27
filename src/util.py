27d8b0686b321526232e576d3182b4f18f57295f
jhondevcode <jhondev.code@gmail.com>
2021-10-27 15:12:21 -0500

    Browse functionality added

--- src/util.py
+++ src/util.py
@@ -1,1 +1,10 @@
-WORKSPACE = str(__file__).replace("/util.py", "")
+from PyQt6.QtGui import QDesktopServices
+from PyQt6.QtCore import QUrl
+
+
+def browse(url: str):
+    """
+        This function uses the Qt core to open a url in the system's
+        default browser.
+    """
+    QDesktopServices.openUrl(QUrl(url))
