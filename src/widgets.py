dab499973ef4c504e5d62f916deb7f926371fe7c
jhondevcode <jhondev.code@gmail.com>
2021-10-27 17:57:26 -0500

    Close window event

--- src/widgets.py
+++ src/widgets.py
@@ -1,10 +1,11 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMenuBar
-from PyQt6.QtGui import QIcon, QDesktopServices
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMenuBar, QMessageBox
+from PyQt6.QtGui import QIcon, QDesktopServices, QCloseEvent
 from PyQt6.QtCore import QUrl
 
 from constant import PROGRAM_NAME, PROGRAM_VERSION, PROGRAM_URL
 from impl import Requester
 from util import browse
+from logger import LOG_DIR, log_file
 
 
 class MainWindow(QMainWindow, Requester):
@@ -47,6 +48,16 @@ class MainWindow(QMainWindow, Requester):
     def init_status_bar(self):
         pass
 
+    def closeEvent(self, event: QCloseEvent) -> None:
+        quit_message = QMessageBox.question(
+            self, "Confirm exit", "Are you sure you want to exit?",
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+            QMessageBox.StandardButton.Yes)
+        if quit_message == QMessageBox.StandardButton.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
 
 class MenuBar(QMenuBar):
 
@@ -62,11 +73,14 @@ class MenuBar(QMenuBar):
         self.file_menu = self.addMenu("&File")
         self.open_action = self.file_menu.addAction(QIcon("resources/svg/open_icon.svg"), "&Open")
         self.open_action.setShortcut("Ctrl+O")
+
         self.save_action = self.file_menu.addAction(QIcon("resources/svg/save_icon.svg"), "&Save")
         self.save_action.setShortcut("Ctrl+S")
         self.file_menu.addSeparator()
+
         self.exit_action = self.file_menu.addAction(QIcon("resources/svg/exit_icon.svg"), "&Exit")
         self.exit_action.setShortcut("Ctrl+Q")
+        self.exit_action.triggered.connect(self.parent.close)
 
     def init_run_menu(self):
         self.run_menu = self.addMenu("&Run")
@@ -78,12 +92,18 @@ class MenuBar(QMenuBar):
     def init_tools_menu(self):
         self.tools_menu = self.addMenu("&Tools")
         self.logs_menu = self.tools_menu.addMenu(QIcon("resources/svg/log_icon.svg"), "&Logs")
+
         self.show_log_action = self.logs_menu.addAction("&Show log")
         self.show_log_action.setShortcut("Shift+L")
+        self.show_log_action.triggered.connect(lambda: browse(log_file))
+
         self.open_logdir_action = self.logs_menu.addAction("&Open logs dir")
         self.open_logdir_action.setShortcut("Shift+O")
+        self.open_logdir_action.triggered.connect(lambda: browse(LOG_DIR))
+
         self.clear_logs_action = self.logs_menu.addAction("&Clear old logs")
         self.clear_logs_action.setShortcut("Shift+D")
+
         self.setting_action = self.tools_menu.addAction(QIcon("resources/svg/settings_icon.svg"), "&Settings")
         self.setting_action.setShortcut("Ctrl+Shift+S")
 
