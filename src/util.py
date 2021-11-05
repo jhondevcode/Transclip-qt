d839289cb392fa07bedb8c1c72a2233af6a43fce
jhondevcode <jhondev.code@gmail.com>
2021-11-05 17:12:10 -0500

    Multilanguage support

--- src/util.py
+++ src/util.py
@@ -1,5 +1,23 @@
 from PyQt6.QtGui import QDesktopServices
 from PyQt6.QtCore import QUrl
+from config import config
+from json import load
+
+
+class LocaleUtil:
+
+    def __init__(self):
+        self.dict = {}
+        file_path = f"resources/locales/{config.get('locale')}"
+        file = open(file_path, mode="r", encoding="utf-8")
+        self.dictionary = load(file)
+        file.close()
+
+    def value(self, key: str) -> str:
+        return self.dictionary[key]
+
+
+locale = LocaleUtil()
 
 
 def browse(url: str):
