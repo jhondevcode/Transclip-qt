b9fdd360db7ea29caa2ca515e2be7be2d28a6a85
jhondevcode <jhondev.code@gmail.com>
2021-11-05 21:22:07 -0500

    Setproctitle implemented

--- src/transclip.py
+++ src/transclip.py
@@ -1,9 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from widgets import MainWindow
+import setproctitle as spt
+from constant import PROGRAM_NAME
 
 
 def main():
+    spt.setproctitle(PROGRAM_NAME)
     app = QApplication(sys.argv)
     window = MainWindow(app)
     window.show()
