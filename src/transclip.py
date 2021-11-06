e2a5be56149922fbff3b8d2bb58ff9c64c2b8847
jhondevcode <jhondev.code@gmail.com>
2021-11-05 21:31:33 -0500

    Added support for sass styles

--- src/transclip.py
+++ src/transclip.py
@@ -3,11 +3,15 @@ from PyQt5.QtWidgets import QApplication
 from widgets import MainWindow
 import setproctitle as spt
 from constant import PROGRAM_NAME
+from qtsass import compile
+from config import config
 
 
 def main():
     spt.setproctitle(PROGRAM_NAME)
     app = QApplication(sys.argv)
+    with open(f"resources/styles/{config.get('style')}", mode="r", encoding="utf-8") as stylesheet:
+        app.setStyleSheet(compile(stylesheet.read()))
     window = MainWindow(app)
     window.show()
     sys.exit(app.exec())
