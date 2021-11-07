2fb7d99197946550586ee6dc4cef5443223a4d3e
jhondevcode <jhondev.code@gmail.com>
2021-11-06 20:58:54 -0500

    Monitor was implemented in visual controls

--- src/widgets.py
+++ src/widgets.py
@@ -22,9 +22,10 @@ class MainWindow(QMainWindow, Requester):
     def __init__(self, app: QApplication):
         QMainWindow.__init__(self)
         Requester.__init__(self)
+        self.monitor = None
         self.app_loop = app
         self.init_window()
-    
+
     def init_window(self):
         self.setWindowTitle(f"{PROGRAM_NAME} {PROGRAM_VERSION}")
         self.setWindowIcon(QIcon("resources/svg/favicon.svg"))
@@ -72,27 +73,91 @@ class MainWindow(QMainWindow, Requester):
         self.state_bar.set_delay(float(config.get("interval")))
 
     def closeEvent(self, event: QCloseEvent) -> None:
-        quit_message = show_question_dialog(self, locale.value("EXIT_DIALOG_TITLE"), locale.value("EXIT_DIALOG_MESSAGE"))
+        quit_message = show_question_dialog(self, locale.value("EXIT_DIALOG_TITLE"),
+                                            locale.value("EXIT_DIALOG_MESSAGE"))
         if quit_message == QMessageBox.StandardButton.Yes:
+            self.stop_monitor()
             event.accept()
         else:
             event.ignore()
 
+    # @pyqtSlot(str)
     def set_source_text(self, text: str):
         if self.source_text_edit is not None:
             self.source_text_edit.setPlainText(text)
 
+    # @pyqtSlot(str)
     def set_target_text(self, text: str):
         self.target_text_edit.setPlainText(text)
 
+    # @pyqtSlot(int)
     def set_words_counter(self, words: int):
         self.state_bar.set_words(words)
 
-    def set_network_state(self, state: bool):
-        if state:
+    def set_network_state(self, state: int):
+        """1 -> connecting, 2 -> connected, 3 -> disconnecting, 4 -> disconnected, 5 -> bad network"""
+        if state == -1:
+            self.state_bar.set_state(locale.value("STATE_LABEL_FAILED"))
+        elif state == 1:
+            self.state_bar.set_state(locale.value("STATE_LABEL_TURNING_ON"))
+        elif state == 2:
             self.state_bar.set_state(locale.value("STATE_LABEL_ON"))
-        else:
+        elif state == 3:
+            self.state_bar.set_state(locale.value("STATE_LABEL_TURNING_OFF"))
+        elif state == 4:
             self.state_bar.set_state(locale.value("STATE_LABEL_OFF"))
+        elif state == 5:
+            self.state_bar.set_state(locale.value("STATE_LABEL_BAD_NETWORK"))
+        else:
+            self.state_bar.set_state(locale.value("STATE_LABEL_UNKNOWN"))
+
+    def start_button_action(self, widgets):
+        try:
+            self.set_network_state(1)
+            widgets[0].setEnabled(False)
+            widgets[1].setEnabled(True)
+            self.start_monitor()
+            self.set_network_state(2)
+        except Exception as ex:
+            self.set_network_state(5)
+            logger.error(ex)
+            widgets[0].setEnabled(True)
+            widgets[1].setEnabled(False)
+
+    def stop_button_action(self, widgets):
+        try:
+            self.set_network_state(3)
+            widgets[0].setEnabled(True)
+            widgets[1].setEnabled(False)
+            self.stop_monitor()
+            self.set_network_state(4)
+        except Exception as ex:
+            self.set_network_state(-1)
+            logger.error(ex)
+            widgets[0].setEnabled(False)
+            widgets[1].setEnabled(True)
+
+    def start_monitor(self):
+        try:
+            from monitor import Monitor
+            self.monitor = Monitor(self)
+            self.monitor.source.connect(self.set_source_text)
+            self.monitor.target.connect(self.set_target_text)
+            self.monitor.words.connect(self.set_words_counter)
+            logger.info("Starting monitor...")
+            self.monitor.start_monitoring()
+        except Exception as ex:
+            logger.error(ex)
+
+    def stop_monitor(self):
+        try:
+            if self.monitor is not None and self.monitor.is_running():
+                logger.info("Stopping monitor...")
+                self.monitor.stop_monitoring()
+            else:
+                logger.warn("Monitor is stopped")
+        except Exception as ex:
+            logger.error(ex)
 
 
 class MenuBar(QMenuBar):
@@ -107,27 +172,39 @@ class MenuBar(QMenuBar):
 
     def init_file_menu(self):
         self.file_menu = self.addMenu(locale.value("MENU_BAR_FILE"))
-        self.open_action = self.file_menu.addAction(QIcon("resources/svg/open_icon.svg"), locale.value("MENU_BAR_FILE_OPEN"))
+        self.open_action = self.file_menu.addAction(QIcon("resources/svg/open_icon.svg"),
+                                                    locale.value("MENU_BAR_FILE_OPEN"))
         self.open_action.setShortcut("Ctrl+O")
 
-        self.save_action = self.file_menu.addAction(QIcon("resources/svg/save_icon.svg"), locale.value("MENU_BAR_FILE_SAVE"))
+        self.save_action = self.file_menu.addAction(QIcon("resources/svg/save_icon.svg"),
+                                                    locale.value("MENU_BAR_FILE_SAVE"))
         self.save_action.setShortcut("Ctrl+S")
         self.file_menu.addSeparator()
 
-        self.exit_action = self.file_menu.addAction(QIcon("resources/svg/exit_icon.svg"), locale.value("MENU_BAR_FILE_EXIT"))
+        self.exit_action = self.file_menu.addAction(QIcon("resources/svg/exit_icon.svg"),
+                                                    locale.value("MENU_BAR_FILE_EXIT"))
         self.exit_action.setShortcut("Ctrl+Q")
         self.exit_action.triggered.connect(self.parent.close)
 
     def init_run_menu(self):
         self.run_menu = self.addMenu(locale.value("MENU_BAR_RUN"))
-        self.start_monitor_action = self.run_menu.addAction(QIcon("resources/svg/start_icon.svg"), locale.value("MENU_BAR_RUN_START_MONITOR"))
+        self.start_monitor_action = self.run_menu.addAction(QIcon("resources/svg/start_icon.svg"),
+                                                            locale.value("MENU_BAR_RUN_START_MONITOR"))
         self.start_monitor_action.setShortcut("Ctrl+Shift+T")
-        self.stop_monitor_action = self.run_menu.addAction(QIcon("resources/svg/stop_icon.svg"), locale.value("MENU_BAR_RUN_STOP_MONITOR"))
+        self.start_monitor_action.triggered.connect(
+            lambda: self.parent.start_button_action((self.start_monitor_action, self.stop_monitor_action)))
+
+        self.stop_monitor_action = self.run_menu.addAction(QIcon("resources/svg/stop_icon.svg"),
+                                                           locale.value("MENU_BAR_RUN_STOP_MONITOR"))
         self.stop_monitor_action.setShortcut("Ctrl+Shift+P")
+        self.stop_monitor_action.setEnabled(False)
+        self.stop_monitor_action.triggered.connect(
+            lambda: self.parent.stop_button_action((self.start_monitor_action, self.stop_monitor_action)))
 
     def init_tools_menu(self):
         self.tools_menu = self.addMenu(locale.value("MENU_BAR_TOOLS"))
-        self.logs_menu = self.tools_menu.addMenu(QIcon("resources/svg/log_icon.svg"), locale.value("MENU_BAR_TOOLS_LOGS"))
+        self.logs_menu = self.tools_menu.addMenu(QIcon("resources/svg/log_icon.svg"),
+                                                 locale.value("MENU_BAR_TOOLS_LOGS"))
 
         self.show_log_action = self.logs_menu.addAction(locale.value("MENU_BAR_TOOLS_LOGS_SHOW"))
         self.show_log_action.setShortcut("Shift+L")
@@ -148,7 +225,8 @@ class MenuBar(QMenuBar):
         self.clipboard_clear_action = self.clipboard_menu.addAction(locale.value("MENU_BAR_TOOLS_CLIPBOARD_CLEAR"))
         self.clipboard_clear_action.triggered.connect(clear)
 
-        self.setting_action = self.tools_menu.addAction(QIcon("resources/svg/settings_icon.svg"), locale.value("MENU_BAR_TOOLS_SETTINGS"))
+        self.setting_action = self.tools_menu.addAction(QIcon("resources/svg/settings_icon.svg"),
+                                                        locale.value("MENU_BAR_TOOLS_SETTINGS"))
         self.setting_action.setShortcut("Ctrl+Shift+S")
 
     def init_help_menu(self):
@@ -156,18 +234,21 @@ class MenuBar(QMenuBar):
         self.help_action = self.help_menu.addAction(QIcon("resources/svg/help_icon.svg"), locale.value("MENU_BAR_HELP"))
         self.help_action.setShortcut("Ctrl+Shift+H")
 
-        self.github_action = self.help_menu.addAction(QIcon("resources/svg/github_icon.svg"), locale.value("MENU_BAR_HELP_GITHUB"))
+        self.github_action = self.help_menu.addAction(QIcon("resources/svg/github_icon.svg"),
+                                                      locale.value("MENU_BAR_HELP_GITHUB"))
         self.github_action.triggered.connect(lambda: browse(PROGRAM_URL))
         self.github_action.setShortcut("Ctrl+Shift+G")
         self.help_menu.addSeparator()
-        
-        self.about_action = self.help_menu.addAction(QIcon("resources/svg/about_icon.svg"), locale.value("MENU_BAR_HELP_ABOUT"))
+
+        self.about_action = self.help_menu.addAction(QIcon("resources/svg/about_icon.svg"),
+                                                     locale.value("MENU_BAR_HELP_ABOUT"))
         self.about_action.setShortcut("Ctrl+Shift+A")
 
     def __delete_old_logs(self):
         files: List[str] = listdir(LOG_DIR)
         if len(files) > 1:
-            quit_message = show_question_dialog(self.parent, locale.value("CLEAR_LOG_DIALOG_TITLE"), locale.value("CLEAR_LOG_DIALOG_MESSAGE"))
+            quit_message = show_question_dialog(self.parent, locale.value("CLEAR_LOG_DIALOG_TITLE"),
+                                                locale.value("CLEAR_LOG_DIALOG_MESSAGE"))
             if quit_message == QMessageBox.StandardButton.Yes:
                 try:
                     for fls in files:
@@ -175,7 +256,8 @@ class MenuBar(QMenuBar):
                         if fls is not log_file_name and isfile(to_remove):
                             remove(to_remove)
                             logger.warn(f"Deleting {to_remove}")
-                    show_info_dialog(self.parent, locale.value("SUCCESSFUL_TITLE"), locale.value("CLEAR_LOG_DIALOG_SUCCESS"))
+                    show_info_dialog(self.parent, locale.value("SUCCESSFUL_TITLE"),
+                                     locale.value("CLEAR_LOG_DIALOG_SUCCESS"))
                 except Exception as ex:
                     logger.error(ex)
                     show_error_dialog(self.parent, "Error", str(ex))
